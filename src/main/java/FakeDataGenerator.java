0a71716998f41240a087484c87eb555a6f13b5f7
Anna Tarankova <tarankovaanna@gmail.com>
2024-06-03 23:56:59 +0300

    Reformat code

--- src/main/java/FakeDataGenerator.java
+++ src/main/java/FakeDataGenerator.java
@@ -6,10 +6,6 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-import static org.example.Birthdate.generateRandomBirthdate;
-import static org.example.Email.generateRandomEmail;
-import static org.example.PhoneNumber.generateUniquePhoneNumber;
-import static org.example.Snils.generateRandomSnils;
 import static org.example.Transliteration.transliterate;
 
 
@@ -25,7 +21,7 @@ public class FakeDataGenerator {
     private static final String DATE_FORMAT = "dd.MM.yyyy";
 
     // Метод для записи в CSV
-    public static void writeToCsv(String[] args) {
+    public static void main(String[] args) {
         // Создание множества для хранения уникальных номеров телефонов
         Set<String> phoneNumbers = new HashSet<>();
         // Создание экземпляра генератора случайных чисел
@@ -72,11 +68,11 @@ public class FakeDataGenerator {
 
 
                 // генерация рандомной даты рождения
-                Date birthdate = generateRandomBirthdate();
+                Date birthdate = dDate();
                 String formattedBirthdate = new SimpleDateFormat(DATE_FORMAT).format(birthdate);
 
                 // генерация рандомного номера телефона
-                String phoneNumber = generateUniquePhoneNumber(phoneNumbers, random);
+                String phoneNumber = phone(phoneNumbers, random);
                 phoneNumbers.add(phoneNumber);
 
 
@@ -86,8 +82,8 @@ public class FakeDataGenerator {
                 String newLastName = lastName.toLowerCase();
                 newLastName = transliterate(newLastName);
 
-                String email = generateRandomEmail(newName, newLastName);
-                String snils = generateRandomSnils(random);
+                String email = email(newName, newLastName);
+                String snils = snils(random);
 
                 String csvLine = String.format("%s;%s;%s;%s;%s\n", fullName, formattedBirthdate, phoneNumber, email, snils);
                 writer.write(csvLine);
@@ -96,5 +92,76 @@ public class FakeDataGenerator {
             e.printStackTrace();
         }
     }
+
+
+        private static final String PHONE_NUMBER_PREFIX = "8999";
+        private static final int PHONE_NUMBER_LENGTH = 7;
+
+        public static String phone(Set<String> phoneNumbers, Random random) {
+            String phoneNumber;
+            do {
+                phoneNumber = PHONE_NUMBER_PREFIX + String.format("%0" + PHONE_NUMBER_LENGTH + "d", random.nextInt((int) Math.pow(10, PHONE_NUMBER_LENGTH)));
+            } while (phoneNumbers.contains(phoneNumber));
+            return phoneNumber;
+        }
+
+    private static final int MIN_AGE = 18;
+    private static final int MAX_AGE = 99;
+
+    public static Date dDate() {
+        long minTimestamp = System.currentTimeMillis() - (MAX_AGE * 365L * 24 * 60 * 60 * 1000);
+        long maxTimestamp = System.currentTimeMillis() - (MIN_AGE * 365L * 24 * 60 * 60 * 1000);
+        long randomTimestamp = minTimestamp + (long) (Math.random() * (maxTimestamp - minTimestamp));
+        return new Date(randomTimestamp);
+    }
+
+    private static final String EMAIL_DOMAIN = "example.com";
+
+    public static String email(String resultName, String resultLastName) {
+        String email = resultName.toLowerCase() + "." + resultLastName.toLowerCase() + "@" + EMAIL_DOMAIN;
+        return email;
+    }
+
+    public static String snils(Random random) {
+        StringBuilder snils = new StringBuilder();
+        for (int i = 0; i < 9; i++) {
+            snils.append(random.nextInt(10));
+        }
+
+        int controlNumber = calculateControlNumber(snils.toString());
+        snils.append(String.format("%02d", controlNumber));
+
+        return formatSnils(snils.toString());
+    }
+
+    // метод для проверки контрольной суммы
+    public static int calculateControlNumber(String snils) {
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += Character.getNumericValue(snils.charAt(i)) * (9 - i);
+        }
+
+        int controlNumber = sum % 101;
+        if (controlNumber == 100) {
+            controlNumber = 0;
+        }
+
+        return controlNumber;
+    }
+
+    // метод для формирования номера снилс
+    public static String formatSnils(String snils) {
+        StringBuilder formattedSnils = new StringBuilder();
+        formattedSnils.append(snils.substring(0, 3));
+        formattedSnils.append("-");
+        formattedSnils.append(snils.substring(3, 6));
+        formattedSnils.append("-");
+        formattedSnils.append(snils.substring(6, 9));
+        formattedSnils.append(" ");
+        formattedSnils.append(snils.substring(9, 11));
+        return formattedSnils.toString();
+    }
+
+            
 
 }
